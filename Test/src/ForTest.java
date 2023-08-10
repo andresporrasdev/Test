0fbc7b3da72797489eb7d3fb117ef1ed1fd01168
andresporrasdev <camilop1730@gmail.com>
2023-08-10 12:19:45 -0400

    Change TestJava for sum of numbers excersice

--- Test/src/ForTest.java
+++ Test/src/ForTest.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
 
 public class ForTest {
 
 	public static void main(String[] args) {
-
-		for (int i=0; i <= 100; i=i+2) {
-			System.out.printf(i+" ");
+		Scanner scan = new Scanner(System.in);
+		int number=0;
+		int sum;
+		for (sum=0; sum<100;) {
+		System.out.println("Give me a number to sum");
+		number=scan.nextInt();
+		sum=sum+number;
+		System.out.println("Done "+sum);
 		}
 	}
 }

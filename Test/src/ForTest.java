6756bcfc74f3a5c447e5da3001db3941df3f5721
andresporrasdev <camilop1730@gmail.com>
2023-08-10 20:37:07 -0400

    Program that add all divisors of a num given by the user

--- Test/src/ForTest.java
+++ Test/src/ForTest.java
@@ -3,14 +3,21 @@ import java.util.Scanner;
 public class ForTest {
 
 	public static void main(String[] args) {
+		int numberGiven;
+		int strictDivisor=0;
+		int sum=0;
 		Scanner scan = new Scanner(System.in);
-		int number=0;
-		int sum;
-		for (sum=0; sum<100;) {
-		System.out.println("Give me a number to sum");
-		number=scan.nextInt();
-		sum=sum+number;
-		System.out.println("Done "+sum);
+		System.out.println("Give me a number");
+		numberGiven=scan.nextInt();
+		//System.out.println(numberGiven);
+		//System.out.println(numberGiven/2);
+		for (int index=1; index<=numberGiven/2;index++) {
+			if (numberGiven % index==0) {
+				sum=index+sum;
+				System.out.printf(index+" ");
+			}
+			
 		}
+		System.out.println("\nThe sum of all distric devisers are "+sum);
 	}
 }
